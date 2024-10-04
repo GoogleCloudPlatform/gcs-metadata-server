16a83b8858787f1556a998aee6d5b6e9fcff41f6
Enzo Falone <enzofalone123@gmail.com>
2024-10-03 18:12:13 -0700

    Implemented /explore/ endpoint for v1

--- internal/api/handler/explore.go
+++ internal/api/handler/explore.go
@@ -1,8 +1,11 @@
 package handler
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 
+	"github.com/GoogleCloudPlatform/gcs-metadata-server/internal/model"
 	"github.com/GoogleCloudPlatform/gcs-metadata-server/internal/repo"
 )
 
@@ -19,5 +22,40 @@ func NewExploreHandler(exploreRepo repo.ExploreRepository) *exploreHandler {
 }
 
 func (e *exploreHandler) HandleExplore(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
+	// Normalize path param
+	path := r.PathValue("path")
+	if len(path) == 0 {
+		path = "/"
+	} else if path[len(path)-1] != '/' {
+		path = path + "/"
+	}
+
+	// Validate sort query param
+	sort := r.URL.Query().Get("sort")
+	if len(sort) == 0 {
+		sort = "size"
+	} else if sort != "size" && sort != "count" {
+		http.Error(w, "Invalid sort parameter, please use 'size' or 'count'", http.StatusBadRequest)
+		return
+	}
+
+	contents, err := e.exploreRepo.GetPathContents(path, sort)
+	if err != nil {
+		log.Printf("Error retrieving path contents: %v", err)
+		http.Error(w, "Internal error", http.StatusInternalServerError)
+		return
+	}
+
+	response := struct {
+		Path     string            `json:"path"`
+		Contents []*model.Metadata `json:"contents"`
+	}{
+		Path:     r.PathValue("path"),
+		Contents: contents,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, "Internal error", http.StatusInternalServerError)
+	}
 }
--- internal/repo/explore.go
+++ internal/repo/explore.go
@@ -1,6 +1,8 @@
 package repo
 
 import (
+	"errors"
+
 	"github.com/GoogleCloudPlatform/gcs-metadata-server/internal/model"
 )
 
@@ -17,7 +19,51 @@ func NewExploreRepository(db *Database) ExploreRepository {
 }
 
 // GetPath retrieves all directory contents of a given path
-// It excludes directories whose size is 0 
+// It excludes directories whose size is 0
 func (e *Explore) GetPathContents(path, sort string) ([]*model.Metadata, error) {
-	return nil, nil
+	if path == "/" {
+		path = "" // handle root
+	}
+
+	queryContent := `
+		SELECT name, size, count
+		FROM directory
+		WHERE
+			name LIKE $1 || '%/' AND
+			NOT name LIKE $1 || '%/%/' AND
+			NOT name LIKE '/' AND
+			size > 0
+		UNION ALL
+		SELECT name, size, 0 as count
+		FROM metadata
+		WHERE
+			name LIKE $1 || '%' AND
+			NOT name LIKE $1 || '%/%'
+	`
+
+	if sort == "count" {
+		queryContent += " ORDER BY count DESC"
+	} else if sort == "size" {
+		queryContent += " ORDER BY size DESC"
+	} else {
+		return nil, errors.New("invalid sort parameter")
+	}
+	queryContent += " LIMIT 100;"
+
+	rows, err := e.DB.Queryx(queryContent, path)
+	if err != nil {
+		return nil, errors.New("query error: " + err.Error())
+	}
+	defer rows.Close()
+
+	var pathContents []*model.Metadata
+	for rows.Next() {
+		var metadata model.Metadata
+		if err := rows.StructScan(&metadata); err != nil {
+			return nil, errors.New("scan error: " + err.Error())
+		}
+		pathContents = append(pathContents, &metadata)
+	}
+
+	return pathContents, nil
 }
