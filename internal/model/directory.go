a8f80ca7ce1158306de27ed6b91e15b009431559
Enzo Falone <enzofalone123@gmail.com>
2024-10-14 17:30:55 -0700

    Updated directory model to schema

--- internal/model/directory.go
+++ internal/model/directory.go
@@ -1,8 +1,11 @@
 package model
 
 type Directory struct {
-	Bucket string `json:"bucket" db:"bucket"`
-	Name   string `json:"name" db:"name"`
-	Size   int64  `json:"size" db:"size"`
-	Count  int64  `json:"count" db:"count"`
+	Bucket       string `json:"bucket" db:"bucket"`
+	Name         string `json:"name" db:"name"`
+	SizeStandard int64  `db:"size_standard"`
+	SizeNearline int64  `db:"size_nearline"`
+	SizeColdline int64  `db:"size_coldline"`
+	SizeArchive  int64  `db:"size_archive"`
+	Count        int64  `json:"count" db:"count"`
 }
